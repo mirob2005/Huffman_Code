75b5369ef3d85e051aac0c53709f77a57b57f2d1
Michael Robertson <mirob2005@gmail.com>
2013-04-13 12:26:24 -0700

    Compressed file being created properly, need to add requireed information to properly decode

--- compress.py
+++ compress.py
@@ -12,24 +12,21 @@ from include.Node import Node
 def compress(inFile,outFile):
     infp = open(inFile,'r')
     text = infp.read()
-    outfp = open(outFile,'w')
+    outfp = open(outFile,'wb')
     
-    nodes = []
+    nodes = {}
     
     for char in text:
-        found = False
-        for node in nodes:
-            if node.key == char:
-                node.value += 1
-                found = True
-        if not found:
-            nodes.append(Node(1,char))
+        if char in nodes:
+            nodes[char].value += 1
+        else:
+            nodes[char] = Node(1,char)
 
-    for node in nodes:
-        print('Node %s has value %s and code %s'%(node.key,node.value,node.code))
+    #for node in nodes.values():
+        #print('Node %s has value %s and code %s'%(node.key,node.value,node.code))
             
     q = PriorityQueue()
-    for node in nodes:
+    for node in nodes.values():
         q.enQueue(node)
     
     min1 = q.deQueue()
@@ -38,24 +35,40 @@ def compress(inFile,outFile):
         q.enQueue(min1+min2)
         min1 = q.deQueue()
         min2 = q.deQueue()
-    print(min1)
+   
+    root = min1
 
-    #groupings = []    
-    #while output:
-    #    groupings.append(output[:8])
-    #    output = output[8:]
-    #    
-    #for group in groupings:
-    #    outfp.write(chr(int(group,2)))
+    output = ''
+    for char in text:
+        if char in nodes:
+            output += nodes[char].code
+        else:
+            print('Char object not found!')
+
+    bitLength = len(output)
+    hexGrp = []    
+    while output:
+        hexGrp.append(output[:8])
+        output = output[8:]
+        
+    output = bytes([int(group,2) for group in hexGrp])
+    
+    size = outfp.write(output)
     infp.close()
     outfp.close()
+    
+    return size
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Usage: python compress.py originalFile.txt compressedFile.txt')
         exit()
     if os.path.exists(sys.argv[1]):
-        compress(sys.argv[1],sys.argv[2])
+        uncompressedSize = os.stat(sys.argv[1]).st_size
+        compressedSize = compress(sys.argv[1],sys.argv[2])
+        print('Uncompressed Size: %s bytes'%uncompressedSize)
+        print('Compressed Size: %s bytes'%compressedSize)
+        print('Precent Space Saved: %f'%(1-(compressedSize/uncompressedSize)))
     else:
         print('Path %s does not exist!'%sys.argv[1])
     
